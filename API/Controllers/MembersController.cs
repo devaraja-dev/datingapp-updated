b14551c8e4594c29e4df3a553c4e9b637959bc59
Devaraja <sdevaraj628@gmail.com>
2026-01-23 16:50:08 +0530

    End of section 8

--- API/Controllers/MembersController.cs
+++ API/Controllers/MembersController.cs
@@ -1,35 +1,38 @@
-using API.Data;
 using API.Entities;
+using API.Interfaces;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-
 namespace API.Controllers
 {
+    [Authorize]
     // Dotnet framework is responsible for creating/ instantiating an new instance
     // of this controller class.
-    public class MembersController(AppDbContext context) : BaseApiController
+    public class MembersController(IMemberRepository memberRepository) : BaseApiController
     {
         [HttpGet]
         // There are multiple different collections in dotnet,
         // Here we are using List<> collection.
-        public async Task<ActionResult<IReadOnlyList<AppUser>>> GetMembers()
+        public async Task<ActionResult<IReadOnlyList<Member>>> GetMembers()
         {
-            var members = await context.Users.ToListAsync();
-
-            return members;
+            return Ok(await memberRepository.GetMembersAsync());
         }
 
-        [Authorize]
+        
         [HttpGet("{id}")] // localhost://5001/api/member/bod-id
         // when id is curly brackets - acts as route parameter 
-        public async Task<ActionResult<AppUser>> GetMember(string id)
+        public async Task<ActionResult<Member>> GetMember(string id)
         {
-            var member = await context.Users.FindAsync(id);
+            var member = await memberRepository.GetMemberByIdAsync(id);
 
             if (member == null) return NotFound();
 
             return member;
+        }
+
+        [HttpGet("{id}/photos")]
+        public async Task<ActionResult<IReadOnlyList<Photo>>> GetPhotosForMemberAsync(string id)
+        {
+            return Ok(await memberRepository.GetPhotosForMemberAsync(id));
         }
         
     }
